8b9cb40edb51debd39d5dcee03b3058fb0c01072
callum wyness <callum@DESKTOP-LND0SA5.localdomain>
2020-06-02 02:09:40 +0100

    build

---
+++ server/sql/reorder.sql
@@ -0,0 +1,15 @@
+SELECT p.FullProductName                           as Name,
+       IFNULL(sk.UnitsInStock, 0)                  as Quantity,
+       TRIM(LEADING '0' from sk.ReorderLevel)      as Level,
+       sk.ReorderAmt                               as Amount,
+       p.PackSize                                  as PkSize,
+       p.PackCost                                  as PkCost,
+       @cost := IFNULL(p.PackSize / p.PackCost, 0) as UtCost,
+       @cost * sk.ReorderAmt                       as Total,
+       p.ProductID                                 as Id,
+       sp.SupplierName                             as Supplier
+FROM Stock sk
+         INNER JOIN Product p ON sk.ProductID = p.ProductID
+         LEFT JOIN Supplier sp ON sp.SupplierID = p.PreferredSuppID
+WHERE p.Deleted = 0 && ReorderAmt > 0
+ORDER BY ${orderBy} ${order} limit ${offset}, ${bufferSize};
---
+++ server/sql/stock.sql
@@ -0,0 +1,15 @@
+select p.ProductId                                as Id,
+       p.ProductName                              as Prod,
+       IFNULL(UnitQuantity, 0)                    as Qty,
+       @cst := IFNULL(p.PackSize / p.PackCost, 0) as Cost,
+       trim(leading '0' from WholeSale) + 0       as WhSale,
+       trim(leading '0' from Retail) + 0          as Rtl,
+       IFNULL(((Retail - @cst) / @cst), 1) * 100  as Profit,
+       DATE_FORMAT(s.LastUpdate, '%y/%m/%d')      as Updated
+from product p
+         left join stockitem s
+                   on s.ProdID = p.ProductID
+         left join category c on p.CategoryID = c.CategoryID
+order by ${orderBy} ${order}
+limit ${offset}, ${bufferSize};
+
