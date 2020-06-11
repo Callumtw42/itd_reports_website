c96a72e4780912e48914f304456a3f2ccd808d3d
callum wyness <callum@DESKTOP-LND0SA5.localdomain>
2020-06-11 02:21:51 +0100

    hide undeeded dbs

--- sql/databases.sql
+++ sql/databases.sql
@@ -1,1 +1,4 @@
-select schema_name from information_schema.SCHEMATA where SCHEMA_NAME != 'information_schema';
+select schema_name
+from information_schema.SCHEMATA
+where SCHEMA_NAME = 'itdepos'
+   OR SCHEMA_NAME = 'itdepos2';
